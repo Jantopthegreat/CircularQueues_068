f352fa8544ca555c787f2d1339015e849222e8ea
Jantopthegreat <fauzangriya@gmail.com>
2023-06-23 15:35:37 +0700

    membuat Menu

--- CircurlarQueues/CircurlarQueues.cpp
+++ CircurlarQueues/CircurlarQueues.cpp
@@ -101,8 +101,18 @@ public:
 int main() {
 	queues q;
 	char ch;
+	while (true) {
+		try {
+			cout << "Menu" << endl;
+			cout << "1. Implement insert operation" << endl;
+			cout << "2. Implement delete operation" << endl;
+			cout << "3. Display values" << endl;
+			cout << "4. Exit" << endl;
+			cout << "Enter your choice (1-4): ";
+			cin >> ch;
+			cout << endl;
 
-	
+
 
 
 
