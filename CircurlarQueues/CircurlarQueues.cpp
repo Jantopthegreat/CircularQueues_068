2f74ee310b3089e545acad028147025c9b91c216
Jantopthegreat <fauzangriya@gmail.com>
2023-06-23 15:05:21 +0700

    membuat constructor remove dan cek apabila antrian kosong

--- CircurlarQueues/CircurlarQueues.cpp
+++ CircurlarQueues/CircurlarQueues.cpp
@@ -17,27 +17,35 @@ public:
 		cout << "Enter a Number: ";
 		cin >> num;
 		cout << endl;
-	
 
-	// Cek Apakah Antrian Penuh // 
-	if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
-		cout << "\nQueue overflow\n";
+
+		// Cek Apakah Antrian Penuh // 
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overflow\n";
+			return;
+		}
+
+		// cek apakah antrian kosong //
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			// Jika REAR berada di ujung terakhir array kembalikan ke awal array //
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
+	}
+void remove(){
+	// cek apakah antrian kosong //
+	if (FRONT == -1) {
+		cout << "Queue Overflor\n";
 		return;
 	}
 
-	// cek apakah antrian kosong //
-	if (FRONT == -1) {
-		FRONT = 0;
-		REAR = 0;
-	}
-	else {
-		// Jika REAR berada di ujung terakhir array kembalikan ke awal array //
-		if (REAR == max - 1)
-			REAR = 0;
-		else
-			REAR = REAR + 1;
-	}
-	queue_array[REAR] = num;
 
 
 
